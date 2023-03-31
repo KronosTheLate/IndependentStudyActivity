4212aa09e83bf9f6f308c76ee1f7c85d8ff88387
KronosTheLate <densbal@live.no>
2023-03-31 10:33:56 +0200

    sync

--- scripts/test.jl
+++ scripts/test.jl
@@ -1,2 +1,21 @@
 using PyCall
+py"1+1"
+##
 DAQC = pyimport("piplates.DAQCplate")
+DAQC2 = pyimport("piplates.DAQC2plate")
+
+DAQC.DAQCversion, DAQC2.DAQC2version
+DAQC.daqcsPresent, DAQC2.daqc2sPresent
+
+pref_dir = joinpath(homedir(), "IndependentStudyActivity")
+if pwd() == pref_dir
+    print("In expected directory: `$pref_dir`")
+else
+    print("Current working directory is different from preferred directory.")
+    print("Changing directory to ", pref_dir)
+    cd(pref_dir)
+end
+datadir = joinpath(pref_dir, "data")
+# os.mkdir(datadir)
+
+DAQC.VerifyADDR(0)  # Useful to check if adress is valid
